e8a321ebbdd1504b35e18916d0867230d86dfa89
sighmon <simon.loffler@gmail.com>
2018-04-27 17:09:32 +0930

    /sbin/reboot for Ubuntu.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -20,7 +20,7 @@ class WelcomeController < ApplicationController
   end
 
   def reboot_server
-    system "sudo reboot"
+    system "sudo /sbin/reboot"
     redirect_to images_path, notice: 'Rebooting...'
   end
 end
